945f74e16344b96dbf6ecb53999ffbbc90861675
jesse <1281772704@qq.com>
2016-01-13 08:21:40 +0800

    Initial Commit

---
+++ SwiftErWeiMa/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftErWeiMa
+//
+//  Created by bsmac1 on 16/1/13.
+//  Copyright © 2016年 bsmac1. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
